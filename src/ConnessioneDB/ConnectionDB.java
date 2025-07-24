d4b194edee632c9efa064d6a29564d572bf4db91
AlessandroFioretto <aless.fioretto@studenti.unina.it>
2025-07-24 19:25:08 +0200

    stampa ciao

--- src/ConnessioneDB/ConnectionDB.java
+++ src/ConnessioneDB/ConnectionDB.java
@@ -1,4 +1,4 @@
-package ConnessioneDB;
+
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/ConnessioneDB/Postgres_JDBC.java
+++ src/ConnessioneDB/Postgres_JDBC.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class Postgres_JDBC {
     public static void main(String[] args) {
-
+        System.out.println("Ciao");
         Connection conn_SQL = null;
 
         //istanzia la classe di connessione al DB
