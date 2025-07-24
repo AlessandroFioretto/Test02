e1684f989f602923d09ade982070866ca501a49d
AlessandroFioretto <aless.fioretto@studenti.unina.it>
2025-07-24 17:23:56 +0200

    Ho aggiunto la stringa s1 e ne ho chiesto la stampa.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao");
+        String s1  = new String("fgidgffd");
+        System.out.println(s1);
     }
+
 }
